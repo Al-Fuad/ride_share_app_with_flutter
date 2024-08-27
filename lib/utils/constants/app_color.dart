6bc84b4005d3808de869788f58afba44250aaa39
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-08-27 19:38:22 +0600

    feat: Add AppColor class

---
+++ lib/utils/constants/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor{
+  static Color primaryColor = Colors.greenAccent;
+  static Color secondaryColor = Colors.greenAccent.shade100;
+  static Color error = Colors.red;
+  static Color success = Colors.green.shade600;
+  static Color warning = Colors.orange.shade600;
+  static Color info = Colors.grey.shade300;
+
+  static Color bgLight = Colors.white;
+}
