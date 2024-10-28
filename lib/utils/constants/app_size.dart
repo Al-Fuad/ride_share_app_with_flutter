ac946a5ac27dc1a3eb57ff5d617b102b985abbed
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-10-28 20:44:46 +0600

    Refactor app_size.dart to add a class for managing app font sizes

---
+++ lib/utils/constants/app_size.dart
@@ -0,0 +1,17 @@
+class AppSize {
+  static double titleXl = 34;
+  static double titleLg = 28;
+  static double titleMd = 24;
+  static double titleSm = 22;
+  static double headlineLg = 20;
+  static double headlineSm = 18;
+  static double subHeadLg = 16;
+  static double subHeadSm = 14;
+  static double bodyLg = 16;
+  static double bodyMd = 14;
+  static double bodySm = 12;
+  static double labelLg = 16;
+  static double labelMd = 14;
+  static double labelSm = 12;
+  static double caption = 12;
+}
