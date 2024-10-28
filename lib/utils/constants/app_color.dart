8adcf52d851a45a9dd8346bc4c2c981eec6d3875
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-10-28 20:43:26 +0600

    Refactor AppColor class and update color constants

--- lib/utils/constants/app_color.dart
+++ lib/utils/constants/app_color.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
-class AppColor{
-  static Color primaryColor = Colors.greenAccent;
+class AppColor {
+  static Color primaryColor = const Color(0xFF08B783);
   static Color secondaryColor = Colors.greenAccent.shade100;
-  static Color error = Colors.red;
+  static Color darkGrey = Colors.grey.shade800;
+  static Color grey = Colors.grey;
+  static Color lightGrey = const Color(0xFFA0A0A0);
+  static Color red = Colors.red;
+  static Color error = red;
   static Color success = Colors.green.shade600;
   static Color warning = Colors.orange.shade600;
   static Color info = Colors.grey.shade300;
+  static Color white = Colors.white;
+  static Color black = Colors.black;
 
-  static Color bgLight = Colors.white;
+  static Color bgLight = white;
 }
