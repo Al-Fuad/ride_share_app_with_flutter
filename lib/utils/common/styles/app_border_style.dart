a7d156d1b0b99881241c6adfa56d4362275e0668
Al-Fuad <117115646+Al-Fuad@users.noreply.github.com>
2024-10-28 20:44:36 +0600

    Refactor app border style to use a common class for input field border

---
+++ lib/utils/common/styles/app_border_style.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppBorderStyle {
+  static OutlineInputBorder inputFieldBorder = OutlineInputBorder(
+    borderSide: const BorderSide(
+      color: Colors.grey,
+      width: .5,
+    ),
+    borderRadius: BorderRadius.circular(10),
+  );
+}
